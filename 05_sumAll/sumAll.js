dba2a5f8448bbaf53c853f505c4469f4bfabfd7b
kmiraie <kavonmiraie@yahoo.com>
2024-07-04 13:21:16 +0900

    added solution 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(...num) {
+    if(num.some(n => typeof n !== 'number')) return 'ERROR'
+    let numbers = [];
+    let start = Math.min(num[0], num[1]);
+    let end = Math.max(num[0], num[1]);
+    for(let i = start; i <= end; i++) {
+        if(i < 0){
+            return 'ERROR'
+        }else{
+            numbers.push(i)
+        }
+    }
+    return numbers.reduce((a,c) => a+c, 0);
 };
 
 // Do not edit below this line
