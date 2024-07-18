d91cf8a9241a1d60ba841478d00e65f612186d40
kmiraie <kavonmiraie@yahoo.com>
2024-07-18 12:56:26 +0900

    added solution 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (pal) {
+    let alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    let cleanedString = pal
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('')
 
+    let reversedString = cleanedString.split('').reverse().join('')
+
+    return cleanedString === reversedString
 };
 
 // Do not edit below this line
